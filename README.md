6cdaaefdfda8d446d1a274e7832191f9910c6800
Colten Tussenbrook <coltenvantussenbrook@gmail.com>
2021-11-23 21:42:57 -0700

    add portfolio custom post type

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Barebones theme. Only used to leverage WP as a CMS. All front end rendering is done through Gatsby and hosted on Netlify.
+Barebones theme. Adds a couple post types, removes some WordPress shenanigans and enqueues what's needed. All front end rendering is done through Gatsby and hosted on Netlify.
--- functions.php
+++ functions.php
@@ -9,4 +9,38 @@ function react_router_rewrite () {
 
 add_action('init', 'react_router_rewrite');
 
+function register_post_type_portfolio () {
+    $labels = array(
+        'name' => 'Portfolio Items',
+        'singular_name' => 'Portfolio Item',
+        'menu_name' => 'Portfolio Items',
+        'add_new' => 'Add New Portfolio Item',
+        'edit_item' => 'Edit Portfolio Item',
+        'new_item' => 'New Portfolio Item',
+        'view_item' => 'View Portfolio Item',
+        'search_items' => 'Search Portfolio Items',
+       );
+
+    $args = array(
+    'labels' => $labels,
+    'has_archive' => true,
+    'public' => true,
+    'hierarchical' => false,
+    'supports' => array(
+        'title',
+        'editor',
+        'excerpt',
+        'custom-fields',
+        'thumbnail',
+        'page-attributes'
+    ),
+    'taxonomies' => array('category'),
+    'rewrite' => array( 'slug' => 'portfolio-items' ),
+    );
+
+    register_post_type( 'portfolio-items', $args );
+}
+
+add_action( 'init', 'register_post_type_portfolio' );
+
 ?>
